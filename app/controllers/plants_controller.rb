2adc6ba94e675115787e7f756a3cea6a869907c9
gianluca perrone <perronegian@hotmail.com>
2021-01-21 19:02:20 +0800

    done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,33 @@
+class PlantsController < ApplicationController
+
+  def create
+
+    @plant = Plant.new(plant_params)
+
+    # Find the parent garden
+    @garden = Garden.find(params[:garden_id])
+
+    # Associate the garden with the plant
+    @plant.garden = @garden
+
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      raise
+    end
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @garden = @plant.garden
+    @plant.destroy
+    redirect_to garden_path(@garden)
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+
+end
